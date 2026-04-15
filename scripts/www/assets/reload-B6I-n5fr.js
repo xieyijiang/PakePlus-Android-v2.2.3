dfddc85b73a6d696086337e890587453ebff14ba
Crabby <390094305@qq.com>
2026-04-15 17:07:09 +0800

    update src file from pakeplus

---
+++ scripts/www/assets/reload-B6I-n5fr.js
@@ -0,0 +1,8 @@
+
+/**
+ * 由 Fantastic-mobile 提供技术支持
+ * Powered by Fantastic-mobile
+ * https://fantastic-mobile.hurui.me/
+ */
+    
+import{d as t,b as n,k as a,_,a as c,o as p}from"./index-D2vdv4_w.js";const u=t({__name:"reload",setup(s,{expose:r}){r();const e=n();a(()=>{e.go(-1)});const o={router:e};return Object.defineProperty(o,"__isScriptSetup",{enumerable:!1,value:!0}),o}});function d(s,r,e,o,l,i){return p(),c("div")}const m=_(u,[["render",d],["__file","/Users/crab7/develop/YYProjects/yy-oms-mobile/src/views/reload.vue"]]);export{m as default};
